e630dc4f2351a49b952d8c2a1a46cddd88104be4
sanikashrivastava <san.shrivastava.2004@gmail.com>
2025-06-23 15:44:03 +0530

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+import os
+
+app = Flask(__name__,
+            template_folder=os.path.join('tp', 'template'),
+            static_folder='static',
+            static_url_path='/static')
+
+# Disable caching of static files during development
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+from app import views  # to avoid circular imports
